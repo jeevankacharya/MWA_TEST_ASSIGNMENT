892adc348d3016b836519688c9048d72d22e7966
Zelalem Zergaw <zola145103@gmail.com>
2020-01-16 23:48:21 -0600

    updated

--- Node js 3/child.js
+++ Node js 3/child.js
@@ -2,11 +2,12 @@ const http = require('http');
 const url = require('url');
 const {fork} = require("child_process");
 
-const port = 4000;
+const port = 8000;
 http.createServer((req,res)=>{
     const num=url.parse(req.url,true).query.n;
 
     const childProcess=fork("fibonacci.js");
+    childProcess.send(parseInt(num));
     childProcess.on("message",r=>{
         const obj ={fib:r};
         res.end(JSON.stringify(obj));
